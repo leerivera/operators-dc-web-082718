9543ce4b2d7803984035db76928ce646c9999e29
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-08-22 21:51:54 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,36 @@
+
 def unsafe?(speed)
-
+  
+   if speed > 40
+     true
+   
+    else if speed < 60
+     true
+    
+    
+   end
+  
+ 
 end
-
-
-
 def not_safe?(speed)
-	
+  speed > 40 ? true : false || speed < 60 ? true : false
+ end
+   
+   
+   
+   
+   
+  
 end
+  
+  
+
+
+
+
+  
+
+
 	
 
 
